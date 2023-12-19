ab6d2f20226214d2d0d76ff07f0a94fb3d7a1292
Ivana Pluharikova <pluharikova@shoptet.cz>
2023-12-19 09:36:49 +0100

    Delete logs

--- cypress/e2e/price_sorting_in_category.cy.js
+++ cypress/e2e/price_sorting_in_category.cy.js
@@ -3,14 +3,12 @@ describe('Řazení produktů od nejdražšího', () => {
     cy.visit('/cypress'); 
     cy.contains('Nejdražší').click();
     cy.get('[data-testid="productCards"]').within(() => {
-      cy.wait(1000); // Přidáno čekání na načtení dat (můžete upravit podle potřeby)
+      cy.wait(1000); 
       cy.get('[data-testid="productCardPrice"]').then(($prices) => {
-        // Získání cen produktů a převedení na čísla (s odstraněním "od")
         const prices = $prices.toArray().map(price => {
           const priceText = price.innerText.replace('Kč', '').replace(',', '.').replace('od ', '');
           return parseFloat(priceText);
         });
-        // Ověření, že první cena je vyšší nebo stejná jako všechny ostatní ceny
         const firstPrice = prices[0];
         for (let i = 1; i < prices.length; i++) {
           cy.log(`Očekáváno, že ${firstPrice} je vyšší nebo stejná jako ${prices[i]}`);
